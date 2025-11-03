320ef0674f1035e7aa213ce0b15068ba5974eadb
armsarg2024-cloud <armsarg2024@gmail.com>
2025-11-04 00:10:38 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,446 @@
+document.getElementById("zx").style.display = "none"
+
+document.getElementById("cc").addEventListener("click", () => {
+
+    document.getElementById("zx").style.display = "block"
+
+})
+let txt = true
+document.getElementById("cx").addEventListener("click", () => {
+    document.getElementById("zx").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx1").style.display = "none"
+
+document.getElementById("cc1").addEventListener("click", () => {
+
+    document.getElementById("zx1").style.display = "block"
+
+})
+let txt1 = true
+document.getElementById("cx1").addEventListener("click", () => {
+    document.getElementById("zx1").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+document.getElementById("zx2").style.display = "none"
+
+document.getElementById("cc2").addEventListener("click", () => {
+
+    document.getElementById("zx2").style.display = "block"
+
+})
+let txt2 = true
+document.getElementById("cx2").addEventListener("click", () => {
+    document.getElementById("zx2").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx3").style.display = "none"
+
+document.getElementById("cc3").addEventListener("click", () => {
+
+    document.getElementById("zx3").style.display = "block"
+
+})
+let txt3 = true
+document.getElementById("cx3").addEventListener("click", () => {
+    document.getElementById("zx3").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx4").style.display = "none"
+
+document.getElementById("cc4").addEventListener("click", () => {
+
+    document.getElementById("zx4").style.display = "block"
+
+})
+let txt4 = true
+document.getElementById("cx4").addEventListener("click", () => {
+    document.getElementById("zx4").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx5").style.display = "none"
+
+document.getElementById("cc5").addEventListener("click", () => {
+
+    document.getElementById("zx5").style.display = "block"
+
+})
+let txt5 = true
+document.getElementById("cx5").addEventListener("click", () => {
+    document.getElementById("zx5").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx6").style.display = "none"
+
+document.getElementById("cc6").addEventListener("click", () => {
+
+    document.getElementById("zx6").style.display = "block"
+
+})
+let txt6 = true
+document.getElementById("cx6").addEventListener("click", () => {
+    document.getElementById("zx6").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx7").style.display = "none"
+
+document.getElementById("cc7").addEventListener("click", () => {
+
+    document.getElementById("zx7").style.display = "block"
+
+})
+let txt7 = true
+document.getElementById("cx7").addEventListener("click", () => {
+    document.getElementById("zx7").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx8").style.display = "none"
+
+document.getElementById("cc8").addEventListener("click", () => {
+
+    document.getElementById("zx8").style.display = "block"
+
+})
+let txt8 = true
+document.getElementById("cx8").addEventListener("click", () => {
+    document.getElementById("zx8").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+document.getElementById("zx9").style.display = "none"
+
+document.getElementById("cc9").addEventListener("click", () => {
+
+    document.getElementById("zx9").style.display = "block"
+
+})
+let txt9 = true
+document.getElementById("cx9").addEventListener("click", () => {
+    document.getElementById("zx9").style.display = "none"
+
+})
+
+
+
+
+
+
+document.getElementById("zx11").style.display = "none"
+
+document.getElementById("cc11").addEventListener("click", () => {
+
+    document.getElementById("zx11").style.display = "block"
+
+})
+let txt11 = true
+document.getElementById("cx11").addEventListener("click", () => {
+    document.getElementById("zx11").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx22").style.display = "none"
+
+document.getElementById("cc22").addEventListener("click", () => {
+
+    document.getElementById("zx22").style.display = "block"
+
+})
+let txt22 = true
+document.getElementById("cx22").addEventListener("click", () => {
+    document.getElementById("zx22").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+document.getElementById("zx33").style.display = "none"
+
+document.getElementById("cc33").addEventListener("click", () => {
+
+    document.getElementById("zx33").style.display = "block"
+
+})
+let txt33 = true
+document.getElementById("cx33").addEventListener("click", () => {
+    document.getElementById("zx33").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx44").style.display = "none"
+
+document.getElementById("cc44").addEventListener("click", () => {
+
+    document.getElementById("zx44").style.display = "block"
+
+})
+let txt44 = true
+document.getElementById("cx44").addEventListener("click", () => {
+    document.getElementById("zx44").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx55").style.display = "none"
+
+document.getElementById("cc55").addEventListener("click", () => {
+
+    document.getElementById("zx55").style.display = "block"
+
+})
+let txt55 = true
+document.getElementById("cx55").addEventListener("click", () => {
+    document.getElementById("zx55").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx66").style.display = "none"
+
+document.getElementById("cc66").addEventListener("click", () => {
+
+    document.getElementById("zx66").style.display = "block"
+
+})
+let txt66 = true
+document.getElementById("cx66").addEventListener("click", () => {
+    document.getElementById("zx66").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx77").style.display = "none"
+
+document.getElementById("cc77").addEventListener("click", () => {
+
+    document.getElementById("zx77").style.display = "block"
+
+})
+let txt77 = true
+document.getElementById("cx77").addEventListener("click", () => {
+    document.getElementById("zx77").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx88").style.display = "none"
+
+document.getElementById("cc88").addEventListener("click", () => {
+
+    document.getElementById("zx88").style.display = "block"
+
+})
+let txt88 = true
+document.getElementById("cx88").addEventListener("click", () => {
+    document.getElementById("zx88").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("zx99").style.display = "none"
+
+document.getElementById("cc99").addEventListener("click", () => {
+
+    document.getElementById("zx99").style.display = "block"
+
+})
+let txt99 = true
+document.getElementById("cx99").addEventListener("click", () => {
+    document.getElementById("zx99").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+document.getElementById("zx100").style.display = "none"
+
+document.getElementById("cc100").addEventListener("click", () => {
+
+    document.getElementById("zx100").style.display = "block"
+
+})
+let txt100 = true
+document.getElementById("cx100").addEventListener("click", () => {
+    document.getElementById("zx100").style.display = "none"
+
+})
+
+
+
+
+
+
+
+
+
+
